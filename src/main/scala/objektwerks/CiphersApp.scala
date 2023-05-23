dcf8a931c448f25d859cef299d112784396471c8
objektwerks <objektwerks@runbox.com>
2023-05-23 13:16:09 -0400

    extracted number pane from cipher app

--- src/main/scala/objektwerks/CiphersApp.scala
+++ src/main/scala/objektwerks/CiphersApp.scala
@@ -2,11 +2,10 @@ package objektwerks
 
 import scalafx.application.JFXApp3
 import scalafx.collections.ObservableBuffer
-import scalafx.geometry.{Insets, Pos}
+import scalafx.geometry.Insets
 import scalafx.scene.Scene
-import scalafx.scene.control.Label
 import scalafx.scene.image.Image
-import scalafx.scene.layout.{BorderPane, GridPane, HBox, Priority}
+import scalafx.scene.layout.BorderPane
 
 import Numbers.*
 
@@ -36,35 +35,3 @@ object View:
   val scene = new Scene:
     root = borderPane
     stylesheets = List("/style.css")
-
-final class NumberPane extends HBox:
-  spacing = 6
-  padding = Insets(6)
-
-  val label = new Label:
-    alignment = Pos.CenterLeft
-    text = "Number:"
-
-  val number = new Label:
-    alignment = Pos.CenterLeft
-    text = "0"
-
-  Model.observableEncoding.onChange { (_, _, newValue) =>
-    if newValue <= 0 then number.text = "0"
-    else
-      val prime = if isPrime(newValue) then s"Yes - #${Model.primeRank(newValue)}" else "No"
-      val star = if isStar(newValue) then "Yes" else "No"
-      val triangular = if isTriangular(newValue) then "Yes" else "No"
-      val factors = listFactors(newValue).mkString(", ")
-      number.text = s"$newValue      Prime: $prime      Star: $star      Triangular: $triangular      Factors: $factors"
-  }
-
-  val grid = new GridPane:
-    hgap = 6
-    vgap = 6
-    padding = Insets(top = 6, right = 6, bottom = 6, left = 6)
-    add(label, columnIndex = 0, rowIndex = 0)
-    add(number, columnIndex = 1, rowIndex = 0)
-
-  children = List(grid)
-  HBox.setHgrow(grid, Priority.Always)
---
+++ src/main/scala/objektwerks/NumberPane.scala
@@ -0,0 +1,39 @@
+package objektwerks
+
+import scalafx.geometry.{Insets, Pos}
+import scalafx.scene.control.Label
+import scalafx.scene.layout.{GridPane, HBox, Priority}
+
+import Numbers.{isPrime, isStar, isTriangular, listFactors}
+
+final class NumberPane extends HBox:
+  spacing = 6
+  padding = Insets(6)
+
+  val label = new Label:
+    alignment = Pos.CenterLeft
+    text = "Number:"
+
+  val number = new Label:
+    alignment = Pos.CenterLeft
+    text = "0"
+
+  Model.observableEncoding.onChange { (_, _, newValue) =>
+    if newValue <= 0 then number.text = "0"
+    else
+      val prime = if isPrime(newValue) then s"Yes - #${Model.primeRank(newValue)}" else "No"
+      val star = if isStar(newValue) then "Yes" else "No"
+      val triangular = if isTriangular(newValue) then "Yes" else "No"
+      val factors = listFactors(newValue).mkString(", ")
+      number.text = s"$newValue      Prime: $prime      Star: $star      Triangular: $triangular      Factors: $factors"
+  }
+
+  val grid = new GridPane:
+    hgap = 6
+    vgap = 6
+    padding = Insets(top = 6, right = 6, bottom = 6, left = 6)
+    add(label, columnIndex = 0, rowIndex = 0)
+    add(number, columnIndex = 1, rowIndex = 0)
+
+  children = List(grid)
+  HBox.setHgrow(grid, Priority.Always)
