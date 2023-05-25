72e2abd1d982bf4565536f505195f140b3d89171
objektwerks <objektwerks@runbox.com>
2023-05-25 07:38:49 -0400

    enhanced model

--- src/main/scala/objektwerks/Model.scala
+++ src/main/scala/objektwerks/Model.scala
@@ -7,7 +7,9 @@ object Model:
   val observableEncodings = ObservableBuffer[Encodings]()
   val primes = Numbers.listPrimes(0 to 1000)
 
-  def addNumber(number: Int): Unit = observableNumbers += Number(primes, number)
+  def addNumber(number: Int): Unit =
+    val candidate = Number(primes, number)
+    if !observableNumbers.contains(candidate) then observableNumbers += Number(primes, number)
 
   def addEncoding(text: String): Unit = observableEncodings += Encodings.encode(text)
 
