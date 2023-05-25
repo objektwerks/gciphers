52a0f76f92c996082d0f07a8e6d9bc5f99029c3f
objektwerks <objektwerks@runbox.com>
2023-05-25 07:29:53 -0400

    refactored model

--- src/main/scala/objektwerks/Model.scala
+++ src/main/scala/objektwerks/Model.scala
@@ -3,16 +3,14 @@ package objektwerks
 import scalafx.beans.property.ObjectProperty
 import scalafx.collections.ObservableBuffer
 
-import Numbers.{findPrimeRank, listPrimes}
-
 object Model:
   val observableNumbers = ObservableBuffer[Number]()
   val observableEncodings = ObservableBuffer[Encodings]()
-  val primes = listPrimes(0 to 1000)
+  val primes = Numbers.listPrimes(0 to 1000)
 
-  def primeRank(prime: Int): Int = findPrimeRank(primes, prime)
+  def addNumber(number: Int): Unit = observableNumbers += Number(primes, number)
 
-  def encode(text: String): Unit = observableEncodings += Encodings.encode(text)
+  def addEncoding(text: String): Unit = observableEncodings += Encodings.encode(text)
 
   def clear(): Unit =
     observableNumbers.clear()
