77fd8c7060192c7a086cbf87eefea6eb8e4eac92
objektwerks <objektwerks@runbox.com>
2023-05-25 07:30:29 -0400

    refactored encodings pane

--- src/main/scala/objektwerks/EncodingsPane.scala
+++ src/main/scala/objektwerks/EncodingsPane.scala
@@ -123,7 +123,7 @@ final class EncodingsPane extends VBox:
     if selectedCells.nonEmpty then 
       val tablePosition = selectedCells.get(0)
       val cellRow = tablePosition.getRow()
-      Model.observableNumbers.value = tablePosition.getTableColumn().getCellData(cellRow).asInstanceOf[Int]
+      Model.addNumber( tablePosition.getTableColumn().getCellData(cellRow).asInstanceOf[Int] )
   }
 
   children = List(tableView)
