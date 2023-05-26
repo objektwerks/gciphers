2e69985c319f8369865719917c6830e4a7fb7fc1
objektwerks <objektwerks@runbox.com>
2023-05-26 13:23:24 -0400

    edited comments

--- src/main/scala/objektwerks/Date.scala
+++ src/main/scala/objektwerks/Date.scala
@@ -7,20 +7,20 @@ object Date:
 
   def remainingDaysInYear(date: LocalDate): String = ( date.lengthOfYear() - date.getDayOfYear ).toString
 
-  def splitYear(date: LocalDate): Int = // (5) + (25) + (20) + (23)
+  def splitYear(date: LocalDate): Int = // (mm) + (dd) + (yy) + (yy)
     val month = date.getMonthValue()
     val day = date.getDayOfMonth()
     val year = date.getYear.toString
     val (leftYear, rightYear) = year.splitAt(2)
     month + day + leftYear.toInt + rightYear.toInt
 
-  def foreachYear(date: LocalDate): Int = // (5) + (25) + 2 + 0 + 2 + 3
+  def foreachYear(date: LocalDate): Int = // (mm) + (dd) + y + y + y + y
     val month = date.getMonthValue()
     val day = date.getDayOfMonth()
     val year = date.getYear.toString.toCharArray.map(c => c.toInt).sum
     month + day + year
 
-  def foreachMonthDayYear(date: LocalDate): Int = // 5 + 2 + 5 + 2 + 0 + 2 + 3
+  def foreachMonthDayYear(date: LocalDate): Int = // m + m + d + d + y + y + y + y
     val month = date.getMonthValue.toString.toCharArray.map(c => c.toInt).sum
     val day = date.getDayOfMonth.toString.toCharArray.map(c => c.toInt).sum
     val year = date.getYear.toString.toCharArray.map(c => c.toInt).sum
