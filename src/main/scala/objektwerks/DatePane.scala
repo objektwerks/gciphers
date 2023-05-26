1aa35786d03dcd2d76a7ff496a33f39e36bf9187
objektwerks <objektwerks@runbox.com>
2023-05-26 06:03:07 -0400

    added date pane

---
+++ src/main/scala/objektwerks/DatePane.scala
@@ -0,0 +1,35 @@
+package objektwerks
+
+import java.time.LocalDate
+
+import scalafx.geometry.{Insets, Pos}
+import scalafx.scene.control.{DatePicker, Label, TitledPane}
+import scalafx.scene.layout.{GridPane, HBox, Priority}
+
+object TitledDatePane:
+  def apply(): TitledPane =
+    new TitledPane:
+      text = "Date"
+      content = DatePane()
+
+final class DatePane extends HBox:
+  spacing = 6
+  padding = Insets(6)
+
+  val dateLabel = new Label:
+    alignment = Pos.CenterLeft
+    text = "Date:"
+
+  val dateField = new DatePicker:
+    hgrow = Priority.Always
+    value = LocalDate.now()
+
+  val grid = new GridPane:
+    hgap = 6
+    vgap = 6
+    padding = Insets(top = 6, right = 6, bottom = 6, left = 6)
+    add(dateLabel, columnIndex = 0, rowIndex = 0)
+    add(dateField, columnIndex = 1, rowIndex = 0)
+
+  children = List(grid)
+  HBox.setHgrow(grid, Priority.Always)
