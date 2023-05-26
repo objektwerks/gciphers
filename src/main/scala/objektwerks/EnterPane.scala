7c3e08767283bc1e9d21a574629b857d8c4c2ae6
objektwerks <objektwerks@runbox.com>
2023-05-26 06:26:32 -0400

    refactored enter pane

--- src/main/scala/objektwerks/EnterPane.scala
+++ src/main/scala/objektwerks/EnterPane.scala
@@ -5,7 +5,7 @@ import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.control.{Alert, Button, Label, TextField, TitledPane}
 import scalafx.scene.control.Alert.AlertType
 import scalafx.scene.input.{KeyCode, KeyEvent}
-import scalafx.scene.layout.{GridPane, HBox, Priority}
+import scalafx.scene.layout.{HBox, Priority}
 
 object TitledEnterPane:
   def apply(): TitledPane =
@@ -14,23 +14,24 @@ object TitledEnterPane:
       content = EnterPane()
 
 final class EnterPane extends HBox:
-  spacing = 6
-  padding = Insets(6)
-
   val textLabel = new Label:
+    padding = Insets(6)
     alignment = Pos.CenterLeft
     text = "Text:"
 
   val textField = new TextField:
+    padding = Insets(6)
     hgrow = Priority.Always
     onKeyReleased = (event: KeyEvent) => if event.code == KeyCode.Enter then Model.addEncoding( text.value )
 
   val numberLabel = new Label:
+    padding = Insets(6)
     alignment = Pos.CenterLeft
     text = "Number:"
 
   val numberField = new TextField:
-    hgrow = Priority.Always
+    padding = Insets(6)
+    prefWidth = 100
     onKeyReleased = (event: KeyEvent) =>
       if event.code == KeyCode.Enter then
         val number = text.value.toIntOption.getOrElse(0)
@@ -44,6 +45,8 @@ final class EnterPane extends HBox:
           }.showAndWait()
 
   val clearButton = new Button:
+    padding = Insets(6)
+    prefWidth = 75
     text = "Clear"
     onAction = { _ =>
       textField.text = ""
@@ -51,15 +54,10 @@ final class EnterPane extends HBox:
       Model.clear()
     }
 
-  val grid = new GridPane:
-    hgap = 6
-    vgap = 6
-    padding = Insets(top = 6, right = 6, bottom = 6, left = 6)
-    add(textLabel, columnIndex = 0, rowIndex = 0)
-    add(textField, columnIndex = 1, rowIndex = 0)
-    add(numberLabel, columnIndex = 2, rowIndex = 0)
-    add(numberField, columnIndex = 3, rowIndex = 0)
-    add(clearButton, columnIndex = 4, rowIndex = 0)
+  val grid = new HBox:
+    spacing = 6
+    padding = Insets(6)
+    children = List(textLabel, textField, numberLabel, numberField, clearButton)
 
   children = List(grid)
   HBox.setHgrow(grid, Priority.Always)
