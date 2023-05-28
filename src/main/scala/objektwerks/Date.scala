12f811cddd3deeab34ab52ee04ff5e246e470892
objektwerks <objektwerks@runbox.com>
2023-05-28 07:08:39 -0400

    refactored split year in date

--- src/main/scala/objektwerks/Date.scala
+++ src/main/scala/objektwerks/Date.scala
@@ -4,17 +4,19 @@ import java.time.LocalDate
 
 object Date:
   type Expression = String
-  type Encoding = String
+  type Encoding = Int
 
   def dayOfYear(date: LocalDate): String = date.getDayOfYear.toString
 
   def remainingDaysInYear(date: LocalDate): String = ( date.lengthOfYear() - date.getDayOfYear ).toString
 
-  def splitYear(date: LocalDate): Int = // (mm) + (dd) + (yy) + (yy)
+  def splitYear(date: LocalDate): (Expression, Encoding) = // (mm) + (dd) + (yy) + (yy)
     val month = date.getMonthValue()
     val day = date.getDayOfMonth()
     val (leftYear, rightYear) = date.getYear.toString.splitAt(2)
-    month + day + leftYear.toInt + rightYear.toInt
+    val expression = s"($month) + ($day) + ($leftYear) + ($rightYear)"
+    val encoding = month + day + leftYear.toInt + rightYear.toInt
+    (expression, encoding)
 
   def splitEachYear(date: LocalDate): Int = // (mm) + (dd) + y + y + y + y
     val month = date.getMonthValue()
