9753007139e8074bde0b8199b006654d29919abd
objektwerks <objektwerks@runbox.com>
2023-05-28 07:20:08 -0400

    refactored split each month day year in date

--- src/main/scala/objektwerks/Date.scala
+++ src/main/scala/objektwerks/Date.scala
@@ -22,12 +22,18 @@ object Date:
     val month = date.getMonthValue()
     val day = date.getDayOfMonth()
     val years = date.getYear.toString.toCharArray
-    val expression = s"($month) + ($day) + ($years(0)) + ($years(1) + ($years(2)) + ($years(3)))"
+    val expression = s"($month) + ($day) + ($years(0)) + ($years(1)) + ($years(2)) + ($years(3)))"
     val encoding = month + day + years.map(c => c.toInt).sum
     (expression, encoding)
 
-  def splitEachMonthDayYear(date: LocalDate): Int = // m + m + d + d + y + y + y + y
-    val month = date.getMonthValue.toString.toCharArray.map(c => c.toInt).sum
-    val day = date.getDayOfMonth.toString.toCharArray.map(c => c.toInt).sum
-    val year = date.getYear.toString.toCharArray.map(c => c.toInt).sum
-    month + day + year
+  def splitEachMonthDayYear(date: LocalDate): (Expression, Encoding) = // m + m + d + d + y + y + y + y
+    val months = date.getMonthValue.toString.toCharArray
+    val days = date.getDayOfMonth.toString.toCharArray
+    val years = date.getYear.toString.toCharArray
+    val expression = s"""
+                      ($months(0)) + ($months(1))
+                      ($days(0)) + ($days(1))
+                      ($years(0)) + ($years(1)) + ($years(2)) + ($years(3))
+                      """.stripMargin
+    val encoding = months.map(c => c.toInt).sum + days.map(c => c.toInt).sum + years.map(c => c.toInt).sum
+    (expression, encoding)
