aa8efca3d3922a9169599d10f42554f8e2c96ee9
objektwerks <objektwerks@runbox.com>
2023-05-28 07:03:39 -0400

    building date dialog

--- src/main/scala/objektwerks/DateDialog.scala
+++ src/main/scala/objektwerks/DateDialog.scala
@@ -2,11 +2,38 @@ package objektwerks
 
 import java.time.LocalDate
 
-import scalafx.scene.control.Dialog
+import scalafx.geometry.{Insets, Pos}
+import scalafx.Includes.*
+import scalafx.scene.control.{ButtonType, Dialog, Label}
+import scalafx.scene.layout.{GridPane, VBox}
 
 final class ChemicalDialog(date: LocalDate) extends Dialog[LocalDate]:
   initOwner(App.stage)
   title = "Date"
-  headerText = "Date Numbers"
+  headerText = "Date Encodings"
 
   println(date)
+
+  val splitYearLabel = new Label:
+    padding = Insets(6)
+    alignment = Pos.CenterLeft
+    text = "Text:"
+
+  val splitYearText = new Label:
+    padding = Insets(6)
+    alignment = Pos.CenterLeft
+    text = "Text:"
+
+  val grid = new GridPane:
+    hgap = 6
+    vgap = 6
+    padding = Insets(top = 6, right = 6, bottom = 6, left = 6)
+    add(splitYearLabel, columnIndex = 0, rowIndex = 0)
+    add(splitYearText, columnIndex = 1, rowIndex = 0)
+
+  dialogPane().buttonTypes = List(ButtonType.Close)
+  dialogPane().content = new VBox:
+    prefWidth = 600
+    prefHeight = 200
+    spacing = 6
+    children = List(grid)
