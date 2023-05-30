49d3d8925b0c5049bbd6cbfd2cf59e2cfceb0482
objektwerks <objektwerks@runbox.com>
2023-05-30 07:16:57 -0400

    added fibonacci to numbers

--- src/main/scala/objektwerks/Numbers.scala
+++ src/main/scala/objektwerks/Numbers.scala
@@ -33,6 +33,15 @@ object Numbers:
       .getOrElse( defaultRankPrime )
     rank
 
+  def fibonacci(n: Long): BigInt =
+    @tailrec
+    def loop(n: Long, a: Long, b: Long): BigInt =
+      n match
+        case 0 => a
+        case _ => loop(n - 1, b, a + b)
+
+    loop(n, 0, 1)
+
   def isStar(n: Int): Boolean =
     val star = ( 6 + Math.sqrt( (24 * n) + 12 ) ) / 6
     (star - star.toInt) == 0
