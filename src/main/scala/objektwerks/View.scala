4dbd546c4a26f698666f350d0cdb64cba60e2e5f
objektwerks <objektwerks@runbox.com>
2023-06-02 07:48:39 -0400

    refactored view

--- src/main/scala/objektwerks/View.scala
+++ src/main/scala/objektwerks/View.scala
@@ -2,18 +2,21 @@ package objektwerks
 
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
-import scalafx.scene.layout.VBox
+import scalafx.scene.layout.{HBox, VBox}
 
 object View:
   val width = 800
   val height = 600
 
   val vbox = new VBox:
+    children = List( TitledNumbersPane(), TitledEncodingsPane(), TitledEnterPane() )
+
+  val hbox = new HBox:
     prefWidth = View.width
     prefHeight = View.height
     padding = Insets(6)
-    children = List( TitledNumbersPane(), TitledEncodingsPane(), TitledDatePane(), TitledEnterPane() )
+    children = List( TitledDatePane(), vbox )
 
   val scene = new Scene:
-    root = vbox
+    root = hbox
     stylesheets = List("/style.css")
