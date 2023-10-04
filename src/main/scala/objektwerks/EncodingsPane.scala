8190946aaa9588571555b9260840e16fc4016682
objektwerks <objektwerks@runbox.com>
2023-10-04 11:31:17 -0400

    enhanced table column

--- src/main/scala/objektwerks/EncodingsPane.scala
+++ src/main/scala/objektwerks/EncodingsPane.scala
@@ -79,6 +79,7 @@ final class EncodingsPane extends VBox:
       new TableColumn[Encodings, Int]:
         text = "Sumerian"
         cellValueFactory = _.value.sumerianProperty
+        cellFactory = (cell, _) => cell.setOnMouseClicked( event => debug(event) )
       ,
       new TableColumn[Encodings, Int]:
         text = "Reverse\nSumerian"
