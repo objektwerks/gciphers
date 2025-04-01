0bbbcd24cfa76815e242e60a210895a2a532b1a1
objektwerks <objektwerks@runbox.com>
2025-04-01 18:54:33 -0400

    enhabnced context

--- src/main/scala/objektwerks/ui/Context.scala
+++ src/main/scala/objektwerks/ui/Context.scala
@@ -55,7 +55,8 @@ final class Context(config: Config):
   val columnTrigonal = config.getString("column.trigonal")
   val columnReverseTrigonal = config.getString("column.reverseTrigonal")
   val columnSingleReduction = config.getString("column.singleReduction")
-  val columnReverseSingleReduction = config.getString("colum.reverseSingleReduction")
+  val columnReverseSingleReduction = config.getString("column.reverseSingleReduction")
+  val columnSatanic = config.getString("column.satanic")
 
   val tabDate = config.getString("tab.date")
   val tabDateEncodings = config.getString("tab.dateEncodings")
