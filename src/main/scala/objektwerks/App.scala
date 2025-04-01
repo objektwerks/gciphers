27a8c7b780044555154c60b81bd12065a221e77e
objektwerks <objektwerks@runbox.com>
2025-04-01 06:35:43 -0400

    added logging to app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -1,10 +1,14 @@
 package objektwerks
 
+import com.typesafe.scalalogging.LazyLogging
+
 import scalafx.application.JFXApp3
 import scalafx.collections.ObservableBuffer
 import scalafx.scene.image.Image
 
-object App extends JFXApp3:
+object App extends JFXApp3 with LazyLogging:
+  logger.info("Starting app ...")
+
   override def start(): Unit =
     stage = new JFXApp3.PrimaryStage:
       scene = View.scene
