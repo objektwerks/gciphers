05bcc92a414f9018efdf2b0747c3e240436e2572
objektwerks <objektwerks@runbox.com>
2025-04-03 08:22:55 -0400

    enhanced store

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -11,5 +11,16 @@ import upickle.default.{read => readJson, write => writeJson}
 final class Store extends LazyLogging:
   val ciphersStorePath = os.home / ".ciphers" / "store"
 
+  private def assertNotInFxThread: Unit = assert( !Platform.isFxApplicationThread, "Store operation called on Fx thread!" )
+
   os.makeDir.all( ciphersStorePath )
   logger.info("Initialized store.")
+
+  def listCipherTexts: List[CipherTexts] =
+    supervised:
+      assertNotInFxThread
+      logger.info(s"List cipher texts.")
+      os.list(ciphersStorePath)
+        .filter { path => path.baseName.nonEmpty }
+        .map { path => readRecipe(s"${path.baseName}.json") }
+        .toList
