48338b7006d0e9cbbf94e58ffaabfdae4fcbfe6f
objektwerks <objektwerks@runbox.com>
2025-04-03 14:06:44 -0400

    edited textx number property

--- src/main/scala/objektwerks/Texts.scala
+++ src/main/scala/objektwerks/Texts.scala
@@ -5,6 +5,6 @@ import upickle.default.ReadWriter as JsonSupport
 import scalafx.beans.property.ObjectProperty
 
 final case class Texts(number: Int, texts: Set[String]) derives CanEqual, JsonSupport:
-  val numberListedProperty = ObjectProperty(number)
+  val numberProperty = ObjectProperty(number)
   val textsProperty = ObjectProperty(texts)
   val fileProperty = ObjectProperty(s"$number.json")
