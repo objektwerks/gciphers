59b8ef2d1a46f78a454807b4359c8762448deb8f
objektwerks <objektwerks@runbox.com>
2025-04-04 14:55:48 -0400

    enhanced texts pane

--- src/main/scala/objektwerks/ui/TextsPane.scala
+++ src/main/scala/objektwerks/ui/TextsPane.scala
@@ -1,8 +1,10 @@
 package objektwerks.ui
 
 import scalafx.geometry.Insets
-import scalafx.scene.control.Label
+import scalafx.scene.control.{Label, TableColumn, TableView}
 import scalafx.scene.layout.VBox
+
+import objektwerks.Texts
 
 final class TextsPane(context: Context, model: Model) extends VBox:
   spacing = 6
@@ -12,3 +14,15 @@ final class TextsPane(context: Context, model: Model) extends VBox:
     padding = Insets(6)
     style = "-fx-font-weight: bold"
     text = context.labelTexts
+
+  val tableView = new TableView[Texts]():
+    columns ++= List(
+      new TableColumn[Texts, Int]:
+        text = context.columnNumber
+        cellValueFactory = _.value.numberProperty
+      ,
+      new TableColumn[Texts, String]:
+        text = context.columnPrimeRank
+        cellValueFactory = _.value.primeRankProperty
+    )
+    items = model.observableNumbers
