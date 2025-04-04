a9d3b10224c7795360e46c805a7f50f8ceb02999
objektwerks <objektwerks@runbox.com>
2025-04-04 14:51:43 -0400

    enhanced texts pane

--- src/main/scala/objektwerks/ui/TextsPane.scala
+++ src/main/scala/objektwerks/ui/TextsPane.scala
@@ -1,8 +1,14 @@
 package objektwerks.ui
 
 import scalafx.geometry.Insets
+import scalafx.scene.control.Label
 import scalafx.scene.layout.VBox
 
 final class TextsPane(context: Context, model: Model) extends VBox:
   spacing = 6
   padding = Insets(6)
+
+  val title = new Label:
+    padding = Insets(6)
+    style = "-fx-font-weight: bold"
+    text = context.labelNumbers
