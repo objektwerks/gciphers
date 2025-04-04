96f2cb97ef058f5c2755e16a3a35a98a14f5786b
objektwerks <objektwerks@runbox.com>
2025-04-04 14:50:54 -0400

    enhanced texts pane

--- src/main/scala/objektwerks/ui/TextsPane.scala
+++ src/main/scala/objektwerks/ui/TextsPane.scala
@@ -1,3 +1,8 @@
 package objektwerks.ui
 
-final class TextsPane
+import scalafx.geometry.Insets
+import scalafx.scene.layout.VBox
+
+final class TextsPane(context: Context, model: Model) extends VBox:
+  spacing = 6
+  padding = Insets(6)
