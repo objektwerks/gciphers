a08bbe5a8caea2e5fa880d254aa0430a15fb6330
objektwerks <objektwerks@runbox.com>
2025-04-04 18:33:52 -0400

    added ordering to texts

--- src/main/scala/objektwerks/Texts.scala
+++ src/main/scala/objektwerks/Texts.scala
@@ -5,6 +5,9 @@ import upickle.default.ReadWriter as JsonSupport
 import scala.collection.mutable
 import scalafx.beans.property.ObjectProperty
 
+object Texts:
+  given Ordering[Texts] = Ordering.by[Texts, Int](t => t.number)
+
 final case class Texts(number: Int, values: mutable.Set[String]) derives CanEqual, JsonSupport:
   val numberProperty = ObjectProperty(number)
   val fileProperty = ObjectProperty(s"$number.json")
